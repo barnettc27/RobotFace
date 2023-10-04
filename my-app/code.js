a7d3cea77d713459ce6c302360817e0b21630d95
barnettc27 <146837599+barnettc27@users.noreply.github.com>
2023-10-04 09:48:09 -0400

    Add files via upload

---
+++ my-app/code.js
@@ -0,0 +1,73 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var mouth = randomNumber(50, 200);
+var eyes = randomNumber(10, 100);
+var ears = 50;
+var antennas = 100;
+fill("lightblue");
+rect(50, 100, 300, 250);
+fill("black");
+rect(mouth, 250, 200, 50);
+fill("black");
+ellipse(125, 175, eyes);
+regularPolygon(275, 175, 5, eyes);
+fill("green");
+rect(350, 150, ears, 100);
+rect(0, 150, ears, 100);
+line(100, 0, 100, antennas);
+line(300, 0, 300, antennas);
+fill(rgb(randomNumber(0, 255), randomNumber(0, 255), randomNumber(0, 255)));
+rect(100, 350, 200, 50);
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
